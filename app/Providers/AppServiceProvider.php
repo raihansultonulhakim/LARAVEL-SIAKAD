5f09366413d5b43fbd54cbbd5b66c3d8375a05b7
Raihan Sultonul Hakim <raihansultonulhakim21@gmail.com>
2026-06-17 08:43:43 +0700

    fix: paksa https scheme

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,24 +2,21 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // <-- PASTIKAN BARIS INI ADA
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        // Paksa semua link asset dan form login pakai HTTPS di server Railway
+        if (config('app.env') === 'production' || env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
