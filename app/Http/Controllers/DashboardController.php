53b21a083a31aab24e71aaa2a94cfffe53a53af3
Raihan Sultonul Hakim <raihansultonulhakim21@gmail.com>
2026-06-17 16:33:25 +0700

    fix: sesuaikan case sensitive model Matakuliah untuk linux

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Mahasiswa;
 use App\Models\Dosen;
-use App\Models\MataKuliah;
+use App\Models\Matakuliah;
 
 class DashboardController extends Controller
 {
@@ -12,7 +12,7 @@ class DashboardController extends Controller
     {
         $totalMahasiswa = Mahasiswa::count();
         $totalDosen = Dosen::count();
-        $totalMataKuliah = MataKuliah::count();
+        $totalMataKuliah = Matakuliah::count();
 
         return view('admin.dashboard_siakad', compact('totalMahasiswa', 'totalDosen', 'totalMataKuliah'));
     }
